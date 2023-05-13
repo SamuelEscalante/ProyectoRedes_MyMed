c9b6a3f1b70182bae1f9d61260be962adc4c3af4
saalesgu <saalesgu@gmail.com>
2023-05-13 18:43:05 -0500

    cambio en headers de php

--- microweb/app-2/editarInventario.php
+++ microweb/app-2/editarInventario.php
@@ -18,13 +18,7 @@ if ($us == "") {
 </head>
 
 <body>
-    <?php
-    session_start();
-    $us = $_SESSION["usuario"];
-    if ($us == "") {
-        header("Location: index.html");
-    }
-    ?>
+
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="jefe.php">MY MED</a>
