53af46dcb2246e5617b0ca9f25339e9d5cc4cf61
saalesgu <saalesgu@gmail.com>
2023-05-16 11:36:33 -0500

    16/05/23

--- microweb/app-2/jefe.php
+++ microweb/app-2/jefe.php
@@ -28,7 +28,7 @@ if (isset($_SESSION["usuario"])) {
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-<h1>app-2</h1>
+
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
